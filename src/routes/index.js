f5aaba4407e80c856ce9d1d3e1d85f426d701969
Nugie kurniawan <nugiebinhar@gmail.com>
2025-12-30 19:27:28 +0700

    api:init

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
 import {Router} from "express";
+import apiRoute from "./api.js";
 
 const route = Router();
 
 route.get("/", (req, res) => {
     res.send("berhasil");
 });
+
+
+// untuk endpoint api
+route.use("/api",apiRoute)
 
 export default route;
