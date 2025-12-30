dc6606eefb1a7b1aa04702fac19e640583a41045
Nugie kurniawan <nugiebinhar@gmail.com>
2025-12-30 20:44:56 +0700

    middleware:membuat middleware untuk handle validation result

---
+++ src/middleware/handleValidation.middleware.js
@@ -0,0 +1,10 @@
+import {validationResult} from "express-validator";
+
+export const handleValidation = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({status: false, error: errors.array()});
+    }
+
+    next();
+};
