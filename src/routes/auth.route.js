b8f2c2757fe7852065575c12d5d86ae6df4ef962
Nugie kurniawan <nugiebinhar@gmail.com>
2025-12-30 22:38:00 +0700

    routes:membuat endpoint untuk check token

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,14 @@
 import {Router} from "express";
 import {regisValid} from "../validation/regis.valid.js";
 import {handleValidation} from "../middleware/handleValidation.middleware.js";
-import {register} from "../controller/auth.controller.js";
+import {checkMe, register} from "../controller/auth.controller.js";
+import { isAuth } from "../middleware/isAuth.middleware.js";
 
 const authRoute = Router();
 
 // untuk register
 authRoute.post("/register", regisValid, handleValidation, register);
 
+// untuk checkMe
+authRoute.get("/me",isAuth,checkMe)
 export default authRoute;
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,12 +1,12 @@
-import {sign, verify} from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import {config} from "dotenv";
 config();
 
 const secret = process.env.SECRET_JWT;
 export const tokenSign = (payload) => {
-    return sign(payload, secret, {expiresIn: "1h"});
+    return jwt.sign(payload, secret, {expiresIn: "1h"});
 };
 
 export const tokenDecode = (token) => {
-    return verify(token, secret);
+    return jwt.verify(token, secret);
 };
