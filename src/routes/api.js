17a87e0b7bd35ee8da110248e3352d5e55583063
Nugie kurniawan <nugiebinhar@gmail.com>
2025-12-30 19:35:56 +0700

    routes:membuat endpoint untuk auth sistem

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,9 @@
 import {Router} from "express";
+import authRoute from "./auth.route.js";
 
 const apiRoute = Router();
+
+// untuk endpoint auth
+apiRoute.use("/auth",authRoute)
 
 export default apiRoute;
