dbfa9ac16786b304b29cc2d5deaf66e065423b51
Nugie kurniawan <nugiebinhar@gmail.com>
2025-12-30 21:52:44 +0700

    controller:membuat controller auth dan membuat untuk register

---
+++ src/controller/auth.controller.js
@@ -0,0 +1,16 @@
+import {registerService} from "../service/auth.service.js";
+
+export const register = async (req, res) => {
+    try {
+        const add = await registerService(req.body);
+        return res.status(200).json({
+            status: true,
+            message: "berhasil menambahkan data",
+        });
+    } catch (error) {
+        return res.status(400).json({
+            status: false,
+            error,
+        });
+    }
+};
