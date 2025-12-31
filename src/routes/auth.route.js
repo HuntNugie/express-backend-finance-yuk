19cdc3ff733ca5177de34e3f125f1099104228d3
Nugie kurniawan <nugiebinhar@gmail.com>
2025-12-31 11:11:11 +0700

    routes:membuat endpoint untuk login

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
 import {regisValid} from "../validation/regis.valid.js";
 import {handleValidation} from "../middleware/handleValidation.middleware.js";
-import {checkMe, register} from "../controller/auth.controller.js";
-import { isAuth } from "../middleware/isAuth.middleware.js";
+import {checkMe, login, register} from "../controller/auth.controller.js";
+import {isAuth} from "../middleware/isAuth.middleware.js";
+import { loginValid } from "../validation/login.valid.js";
 
 const authRoute = Router();
 
@@ -10,5 +11,8 @@ const authRoute = Router();
 authRoute.post("/register", regisValid, handleValidation, register);
 
 // untuk checkMe
-authRoute.get("/me",isAuth,checkMe)
+authRoute.get("/me", isAuth, checkMe);
+
+// untuk login
+authRoute.post("/login",loginValid,handleValidation,login)
 export default authRoute;
--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,8 +1,14 @@
 import { hash,compare } from "bcrypt";
 
 const salt = 10;
+// unutk hashing password
 export const hashPass = async(string)=>{
     const hashing = await hash(string,salt);
     return hashing;
 }
 
+// untuk verifikasi password benar atau salah
+export const comparePass = async(password,passwordHash)=>{
+    const check = await compare(password,passwordHash);
+    return check
+}
