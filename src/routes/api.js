b4b00119ffcd34244f4286fb4080035fb27b4041
Nugie kurniawan <nugiebinhar@gmail.com>
2026-01-05 18:25:46 +0700

    routes:membuat route untuk wallet

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,12 @@
 import {Router} from "express";
 import authRoute from "./auth.route.js";
+import walletRoute from "./wallet.route.js";
 
 const apiRoute = Router();
 
 // untuk endpoint auth
 apiRoute.use("/auth",authRoute)
 
+// untuk endpoint yang berhubungan dengan account
+apiRoute.use("/wallet",walletRoute)
 export default apiRoute;
